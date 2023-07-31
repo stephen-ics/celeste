9ef4fdd3e522dfc4c466c033584a51bf278099a2
stephen-ics <stephenni1234@gmail.com>
2023-07-30 20:28:09 -0400

    ast test -> stringifying ast outputs

--- ast/ast.go
+++ ast/ast.go
@@ -1,9 +1,23 @@
 package ast
 
-import "interpreter/token"
+import (
+	"interpreter/token"
+	"bytes"
+)
+
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -42,8 +56,9 @@ type Identifier struct {
 	Value string
 }
 
-func (i *Identifier) expressNode() {}
-func (i *Identifier) TokenLiteral() string {return i.Token.Literal}
+func (i *Identifier) expressionNode() {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) String() string { return i.Value }
 
 type ReturnStatement struct {
 	Token token.Token // the token.RETURN token
@@ -52,3 +67,50 @@ type ReturnStatement struct {
 
 func (rs *ReturnStatement) statementNode() {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+
+type ExpressionStatement struct {
+	Token token.Token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+	
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.returnValue != nil {
+		out.WriteString((rs.returnValue.String()))
+	}
+	
+	out.WriteString(";")
+
+	return out.String()
+}
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+
+	return ""
+}
+
