59c095d10377348fa28368416808cfee0d7ab491
stephen-ics <stephenni1234@gmail.com>
2023-08-01 01:41:43 -0400

    evaluated all bool infix operators (!= ==)

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -94,6 +94,10 @@ func evalInfixExpression(operator string, left, right object.Object) object.Obje
 	switch {
 	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
 		return evalIntegerInfixExpression(operator, left, right)
+	case operator == "==":
+		return nativeBoolToBooleanObject(left == right)
+	case operator == "!=":
+		return nativeBoolToBooleanObject(left != right)
 	default:
 		return NULL
 	}
