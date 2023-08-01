0ce4f5847f0f2274463035e20668dc08a847ce45
stephen-ics <stephenni1234@gmail.com>
2023-08-01 11:43:53 -0400

    added stringliteral to ast + parsed and tested

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -112,7 +112,7 @@ func (l *Lexer) NextToken() token.Token {
 			tok = newToken(token.LBRACE, l.ch)
 		case '}':
 			tok = newToken(token.RBRACE, l.ch)
-		case '"':
+		case '"': // Add support for \n,\t!, raise an error when reaching the end of an input
 			tok.Type = token.STRING
 			tok.Literal = l.readString()
 		default:
