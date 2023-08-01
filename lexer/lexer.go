88dfb4ba4d79b1c5237fc63a2cc1f192fca73ba8
stephen-ics <stephenni1234@gmail.com>
2023-08-01 11:31:06 -0400

    lexing strings!

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -112,6 +112,9 @@ func (l *Lexer) NextToken() token.Token {
 			tok = newToken(token.LBRACE, l.ch)
 		case '}':
 			tok = newToken(token.RBRACE, l.ch)
+		case '"':
+			tok.Type = token.STRING
+			tok.Literal = l.readString()
 		default:
 			if isLetter(l.ch) {
 				tok.Literal = l.readIdentifier()
@@ -133,6 +136,18 @@ func (l *Lexer) NextToken() token.Token {
 	return tok
 }
 
+func (l *Lexer) readString() string {
+	position := l.position + 1
+	for {
+		l.readChar()
+		
+		if l.ch == '"' || l.ch == 0 {
+			break
+		}
+	}
+
+	return l.input[position:l.position]
+}
 
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
