58c6d5754916d746ff75634657154eef5c3d5a98
stephen-ics <stephenni1234@gmail.com>
2023-08-02 15:57:53 -0400

    passed int arith, compile, add constant, emit

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -12,13 +12,43 @@ type Compiler struct {
 }
 
 func New() *Compiler {
-	return &Compiler{
+	return &Compiler {
 		instructions: code.Instructions{},
 		constants: []object.Object{},
 	}
 }
 
 func (c *Compiler) Compile(node ast.Node) error {
+	switch node := node.(type) {
+	case *ast.Program:
+		for _, s := range node.Statements {
+			err := c.Compile(s)
+			if err != nil {
+				return err
+			}
+		}
+	case *ast.ExpressionStatement:
+		err := c.Compile(node.Expression)
+		if err != nil {
+			return err
+		}
+
+	case *ast.InfixExpression:
+		err := c.Compile(node.Left)
+		if err != nil {
+			return err
+		}
+
+		err = c.Compile(node.Right)
+		if err != nil {
+			return err
+		}
+
+	case *ast.IntegerLiteral:
+		integer := &object.Integer{Value: node.Value}
+		c.emit(code.OpConstant, c.addConstant(integer))
+	}
+	
 	return nil
 }
 
@@ -29,7 +59,25 @@ func (c *Compiler) Bytecode() *Bytecode {
 	}
 }
 
+func (c *Compiler) addConstant(obj object.Object) int {
+	c.constants = append(c.constants, obj)
+	return len(c.constants) - 1 // Returns the index of the newly added object
+}
+
+func (c *Compiler) emit(op code.Opcode, operands ...int) int {
+	ins := code.Make(op, operands...) //The index of the constant is then used as an operand for the OpConstant instruction
+	pos := c.addInstruction(ins) // Returns the position of the newly added
+	return pos
+}
+
+func (c *Compiler) addInstruction(ins []byte) int {
+	posNewInstruction := len(c.instructions) // Position of the newly added instruction
+	c.instructions = append(c.instructions, ins...)
+	return posNewInstruction // Why not -1?
+}
+
 type Bytecode struct {
 	Instructions code.Instructions
 	Constants []object.Object
 }
+
--- parser/parser.go
+++ parser/parser.go
@@ -211,7 +211,7 @@ func (p *Parser) parseIdentifier() ast.Expression {
 func (p *Parser) parseIntegerLiteral() ast.Expression {
 	lit := &ast.IntegerLiteral{Token: p.curToken}
 
-	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64) 
 	if err != nil {
 		msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
 		p.errors = append(p.errors, msg)
