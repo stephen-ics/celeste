9250c9be1b4d2188e194b9e7d4dffd3a36e088ac
stephen-ics <stephenni1234@gmail.com>
2023-08-01 22:34:18 -0400

    print function -> complete!!!

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,6 +1,7 @@
 package evaluator
 
 import (
+	"fmt"
 	"interpreter/object"
 )
 
@@ -93,6 +94,15 @@ var builtins = map[string]*object.Builtin{
 			newElements[length] = args[1]
 
 			return &object.Array{Elements: newElements}
+		},
+	},
+	"puts": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
 		},
 	},
 }
