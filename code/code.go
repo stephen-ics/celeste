b938c49d17b322ad090315ce1613eabd3141371f
stephen-ics <stephenni1234@gmail.com>
2023-08-04 15:34:56 -0400

    compiler + vm supports binding + retrieval

--- code/code.go
+++ code/code.go
@@ -65,6 +65,8 @@ const (
 	OpJumpNotTruthy
 	OpJump
 	OpNull
+	OpSetGlobal
+	OpGetGlobal
 )
 
 type Definition struct {
@@ -89,6 +91,8 @@ var definitions = map[Opcode]*Definition { // This associates the operator (by o
 	OpJumpNotTruthy: {"OpJumpNotTruthy", []int{2}},
 	OpJump: {"OpJump", []int{2}},
 	OpNull: {"OpNull", []int{}},
+	OpGetGlobal: {"OpGetGlobal", []int{2}},
+	OpSetGlobal: {"OpSetGlobal", []int{2}},
 }
 
 func Lookup(op byte) (*Definition, error) {
--- compiler/compiler.go
+++ compiler/compiler.go
@@ -17,6 +17,7 @@ type Compiler struct {
 	constants []object.Object // Generally a read only data sturction, ensure that the constant values follow an LIFO order to keep track of each index, as they are the operands that refer to these values
 	lastInstruction EmittedInstruction
 	previousInstruction EmittedInstruction
+	symbolTable *SymbolTable
 }
 
 type Bytecode struct {
@@ -31,6 +32,7 @@ func New() *Compiler {
 		constants: []object.Object{},
 		lastInstruction: EmittedInstruction{},
 		previousInstruction: EmittedInstruction{}, // The instruction before the last instruction
+		symbolTable: NewSymbolTable(),
 	}
 }
 
@@ -156,6 +158,21 @@ func (c *Compiler) Compile(node ast.Node) error {
 				return err
 			}
 		}
+	
+	case *ast.LetStatement:
+		err := c.Compile(node.Value)
+		if err != nil {
+			return err
+		}
+		symbol := c.symbolTable.Define(node.Name.Value)	
+		c.emit(code.OpSetGlobal, symbol.Index)
+	
+	case *ast.Identifier:
+		symbol, ok := c.symbolTable.Resolve(node.Value)
+		if !ok {
+			return fmt.Errorf("Undefined variable %s", node.Value)
+		}
+		c.emit(code.OpGetGlobal, symbol.Index)
 
 	case *ast.IntegerLiteral:
 		integer := &object.Integer{Value: node.Value}
