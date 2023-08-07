19b763e99c49079779d72978ac1072f8708dab1b
stephen-ics <stephenni1234@gmail.com>
2023-08-07 18:08:19 -0400

    added benchmark to measure performance times!

---
+++ benchmark/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"time"
+	"compiler/compiler"
+	"compiler/evaluator"
+	"compiler/lexer"
+	"compiler/object"
+	"compiler/parser"
+	"compiler/vm"
+)
+
+var engine = flag.String("engine", "vm", "use 'vm' or 'eval'") // engine is the pointer to a string variable "engine", "vm is the default value", "use vm or eval" is a brief description of what the "engine" flag represents, it will be displayed when the user runs "-invalid option"
+
+var input = `
+let fibonacci = fn(x) {
+	if (x == 0) {
+		0
+	} else {
+		if (x == 1) {
+			return 1;
+		} else {
+			fibonacci(x - 1) + fibonacci(x - 2);
+		}
+	}
+};
+fibonacci(35);
+`
+
+func main() {
+	flag.Parse()
+
+	var duration time.Duration
+	var result object.Object
+
+	l := lexer.New(input)
+	p := parser.New(l)
+	program := p.ParseProgram()
+
+	if *engine == "vm" {
+		comp := compiler.New()
+		err := comp.Compile(program)
+		if err != nil {
+			fmt.Printf("compiler error: %s", err)
+			return
+		}
+
+		machine := vm.New(comp.Bytecode())
+
+		start := time.Now()
+
+		err = machine.Run()
+		if err != nil {
+			fmt.Printf("vm error: %s", err)
+			return
+		}
+
+		duration = time.Since(start)
+		result = machine.LastPoppedStackElem()
+	} else {
+		env := object.NewEnvironment()
+		start := time.Now()
+		result = evaluator.Eval(program, env)
+		duration = time.Since(start)
+	}
+
+	fmt.Printf(
+		"engine=%s, result=%s, duration=%s\n",
+		*engine,
+		result.Inspect(),
+		duration)
+}
